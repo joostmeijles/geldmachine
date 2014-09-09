ac1cc7607eabd9ba8e6df1b6a79a6f0052291794
joostmeijles <joost@meijles.com>
2014-09-09 21:47:52 +0200

    Add FSharp.Data

--- Program.fs
+++ Program.fs
@@ -1,7 +1,6 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
 
-[<EntryPoint>]
+[<EntryPoint;STAThread>]
 let main argv = 
     printfn "%A" argv
     0 // return an integer exit code
