df0687137c98882250d0291dfe89c905760ea23d
joostmeijles <joost@meijles.com>
2014-09-20 15:20:48 +0200

    Some better charting

--- Charting.fs
+++ Charting.fs
@@ -1,2 +1,37 @@
 ﻿module GeldMachine.Charting
 
+open FSharp.Charting
+open GeldMachine.Data
+open System.Windows.Forms
+open System.Drawing
+
+type ChartControl (rows:StockData.Row seq) as self = 
+    inherit UserControl()
+    
+    let toHLOC (data:StockData.Row) = data.Date, data.High, data.Low, data.Open, data.Close
+    let minRows rows = Seq.min (Seq.map (fun (r:StockData.Row) -> r.Low) rows)
+    let maxRows rows = Seq.max (Seq.map (fun (r:StockData.Row) -> r.High) rows)
+
+    let candleStickChart =
+        let min = float (minRows rows)
+        let max = float (maxRows rows)
+        let HLOC = Seq.map toHLOC rows  
+        Chart.Candlestick(HLOC).WithYAxis(Min = min, Max = max)
+
+    let barChart =
+        let V = Seq.map (fun (r:StockData.Row) -> r.Date, r.Volume) rows
+        Chart.Column(V)
+
+    do
+        let pricingChart = new ChartTypes.ChartControl(candleStickChart, Dock = DockStyle.Fill)
+        //self.Controls.Add(pricingChart)
+
+        let volumeChart = new ChartTypes.ChartControl(barChart, Dock = DockStyle.Bottom)
+        //self.Controls.Add(volumeChart)
+
+        let combine = Chart.Rows [candleStickChart; barChart]
+        let combineChart = new ChartTypes.ChartControl(combine, Dock = DockStyle.Fill)
+        //combineChart |> FSharp.Charting
+        self.Controls.Add(combineChart)
+
+        
--- Program.fs
+++ Program.fs
@@ -1,37 +1,24 @@
 ﻿open System
 open System.Windows.Forms
 open System.Drawing
-open FSharp.Charting
+
+open GeldMachine.Charting
 open GeldMachine.Data
 
 let printStockData (data:StockData) =
     printfn "%A" data.Headers
     Seq.take 10 data.Rows |> Seq.iter (fun d -> printfn "%A " d)
 
-let toHLOC (data:StockData.Row) = data.Date, data.High, data.Low, data.Open, data.Close
-let minRows rows = Seq.min (Seq.map (fun (r:StockData.Row) -> r.Low) rows)
-let maxRows rows = Seq.max (Seq.map (fun (r:StockData.Row) -> r.High) rows)
-
-let candleStickChart rows =
-    let min = float (minRows rows)
-    let max = float (maxRows rows)
-    let HLOC = Seq.map toHLOC rows  
-    Chart.Candlestick(HLOC).WithYAxis(Min = min, Max = max)
 
 let gspc = getStockData "^GSPC"
-let msft = getStockData "MSFT"
-
-let msftChart = candleStickChart (Seq.take 60 msft.Rows)
+let msft = 
+    let data = getStockData "MSFT"
+    Seq.take 60 data.Rows
 
 [<EntryPoint>]
 let main argv = 
-    printStockData msft
-    printStockData gspc
-
-    //Console.ReadLine() |> ignore
-
     let form = new Form(Visible = true, TopMost = true, Width = 700, Height = 500)
-    form.Controls.Add(new ChartTypes.ChartControl(msftChart, Dock = DockStyle.Fill))
+    form.Controls.Add(new ChartControl(msft, Dock = DockStyle.Fill))
     Application.Run(form);
 
     0 //exit code
