79aa1000cd34cdc5a94233a7b8e54c5aead1fa6f
joostmeijles <joost@meijles.com>
2014-09-20 22:26:09 +0200

    Clean up Charting a bit

--- Charting.fs
+++ Charting.fs
@@ -11,10 +11,10 @@ type ChartControl (rows:OHLC seq, sphs:list<OHLC>, spls:list<OHLC>) as self =
     inherit UserControl()
     
     let toHLOC (data:OHLC) = data.Date, data.High, data.Low, data.Open, data.Close
-    let minRows rows = Seq.min (Seq.map (fun (r:OHLC) -> r.Low) rows)
-    let maxRows rows = Seq.max (Seq.map (fun (r:OHLC) -> r.High) rows)
-    let sphIndices = Seq.map (fun (r:OHLC) -> Seq.findIndex (fun (a:OHLC) -> r.Date = a.Date) rows) sphs
-    let splIndices = Seq.map (fun (r:OHLC) -> Seq.findIndex (fun (a:OHLC) -> r.Date = a.Date) rows) spls
+    let minRows rows = Seq.min (Seq.map (fun r -> r.Low)  rows)
+    let maxRows rows = Seq.max (Seq.map (fun r -> r.High) rows)
+    let sphIndices = Seq.map (fun r -> Seq.findIndex (fun a -> r.Date = a.Date) rows) sphs
+    let splIndices = Seq.map (fun r -> Seq.findIndex (fun a -> r.Date = a.Date) rows) spls
 
     let priceChart =
         let min = float (minRows rows)
@@ -23,7 +23,7 @@ type ChartControl (rows:OHLC seq, sphs:list<OHLC>, spls:list<OHLC>) as self =
         Chart.Candlestick(HLOC).WithYAxis(Min = min, Max = max)
 
     let volumeChart =
-        let V = Seq.map (fun (r:OHLC) -> r.Date, r.Volume) rows
+        let V = Seq.map (fun r -> r.Date, r.Volume) rows
         Chart.Column(V)
 
     let findControl (controls:Control.ControlCollection) =
@@ -34,6 +34,14 @@ type ChartControl (rows:OHLC seq, sphs:list<OHLC>, spls:list<OHLC>) as self =
                 found <- Some(c :?> Chart)
         found   
 
+    let addSwingpointAnnotation (chart:Chart) spIndices text =
+        for i in spIndices do 
+            let dp = chart.Series.[0].Points.[i]
+            let ta = new TextAnnotation()
+            ta.Text <- text
+            ta.AnchorDataPoint <- dp
+            chart.Annotations.Add(ta) 
+
     do
         let combine = Chart.Rows [priceChart; volumeChart]
         let combineChart = new ChartTypes.ChartControl(combine, Dock = DockStyle.Fill)
@@ -41,19 +49,8 @@ type ChartControl (rows:OHLC seq, sphs:list<OHLC>, spls:list<OHLC>) as self =
         let c = findControl combineChart.Controls
         match c with
         | Some(c) ->
-            for sphi in sphIndices do 
-                let dp = c.Series.[0].Points.[sphi]
-                let ta = new TextAnnotation()
-                ta.Text <- "SPH"
-                ta.AnchorDataPoint <- dp
-                c.Annotations.Add(ta)
-
-            for spli in splIndices do 
-                let dp = c.Series.[0].Points.[spli]
-                let ta = new TextAnnotation()
-                ta.Text <- "SPL"
-                ta.AnchorDataPoint <- dp
-                c.Annotations.Add(ta)
+            addSwingpointAnnotation c sphIndices "SPH"
+            addSwingpointAnnotation c splIndices "SPL"
         | None -> ignore()
 
-        self.Controls.Add(combineChart)     
+        self.Controls.Add(combineChart)
--- Swingpoint.fs
+++ Swingpoint.fs
@@ -29,21 +29,3 @@ let private getSPs (data:OHLC list) (property:OHLC->decimal) (compare:decimal->d
 
 let getSPLs (data:OHLC list) = getSPs data (fun row -> row.Low) (fun current best -> current > best)
 let getSPHs (data:OHLC list) = getSPs data (fun row -> row.High) (fun current best -> current < best)
- 
-(*
-let rec getSPHs (rows:StockData.Row list) sphs =
-    match rows with
-    | h :: t ->
-        if Seq.length t >= 5 then 
-            let next5 = Seq.take 5 t
-            let high5 = Seq.map (fun (r:StockData.Row) -> r.High) next5
-            let max5  = Seq.max high5
-            if h.High > max5
-                then getSPHs (List.ofSeq (Seq.skip 6 rows)) (Seq.append [(SPH, h)] sphs) 
-                else getSPHs (List.ofSeq t) sphs
-        else sphs
-    | _ -> sphs
-
-let getSwingpoints (rows:StockData.Row seq) =
-    getSPHs (List.ofSeq rows) []
-*)
