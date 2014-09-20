3a8a8033458f4436ce1c0f0a3ba6c8fa1bca9312
joostmeijles <joost@meijles.com>
2014-09-20 21:27:48 +0200

    Add Tests

---
+++ Tests/Tests.fs
@@ -0,0 +1,60 @@
+﻿module Tests
+
+open System
+
+open FsCheck
+open FsCheck.Xunit
+
+type OHLC = {
+    Date     : DateTime
+    Open     : decimal
+    High     : decimal
+    Low      : decimal
+    Close    : decimal
+    Volume   : int64
+    Adjusted : decimal }
+
+let private getSPs (data:OHLC list) (property:OHLC->decimal) (compare:decimal->decimal->bool) = 
+    match data with
+    | h :: t ->
+        let mutable potentialSwingPoint = Seq.head data
+        let mutable successive = 0
+        let mutable sps = []
+
+        for row in data do
+            if(compare (property row) (property potentialSwingPoint)) then
+                successive <- successive + 1
+                if(successive >= 6) then
+                    sps <- List.append [potentialSwingPoint] sps
+                    potentialSwingPoint <- row
+                    successive <- 0
+            else
+                potentialSwingPoint <- row
+                successive <- 0 
+        sps
+    | _ -> []
+
+let getSPLs (data:OHLC list) = getSPs data (fun row -> row.Low) (fun current best -> current > best)
+let getSPHs (data:OHLC list) = getSPs data (fun row -> row.High) (fun current best -> current < best)
+
+[<Property(StartSize=100, EndSize=1000)>]
+let ``SPL all higher`` (a:list<OHLC>) = 
+    let mutable res = []
+    let spls = getSPLs a
+    for s in spls do
+        let i    = List.findIndex (fun j -> j = s) a
+        let lst  = Seq.take 5 (Seq.skip (i+1) a)
+        let lows = Seq.map (fun r -> r.Low) lst
+        res <- List.append [((Seq.min lows) > s.Low)] res
+    Seq.forall (fun r -> r = true) res
+
+[<Property(StartSize=100, EndSize=1000)>]
+let ``SPH all lower`` (a:list<OHLC>) = 
+    let mutable res = []
+    let sphs = getSPHs a
+    for s in sphs do
+        let i     = List.findIndex (fun j -> j = s) a
+        let lst   = Seq.take 5 (Seq.skip (i+1) a)
+        let highs = Seq.map (fun r -> r.High) lst
+        res <- List.append [((Seq.max highs) < s.High)] res
+    Seq.forall (fun r -> r = true) res
